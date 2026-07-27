f669ebcec6af45599216963ff8a280c71200b28c
Huynh Gia Buu <buub1309120@gmail.com>
2026-07-27 13:18:52 +0700

    chore: update openpi cask to v0.2.5

--- Casks/openpi.rb
+++ Casks/openpi.rb
@@ -1,8 +1,16 @@
 cask "openpi" do
-  version "0.2.4"
-  sha256 "3f73af50dcd7914c8d7041fcf528aecf31db35fecde040272af5d18c97222646"
+  version "0.2.5"
 
-  url "https://github.com/heyhuynhgiabuu/openpi/releases/download/v#{version}/OpenPi-#{version}-arm64.dmg"
+  on_arm do
+    sha256 "a775c7b22896f0e9e10b70728a2896e0696f7919e2727f732444afb5a9ae9a8d"
+    url "https://github.com/heyhuynhgiabuu/openpi/releases/download/v#{version}/OpenPi-#{version}-arm64.dmg"
+  end
+
+  on_intel do
+    sha256 "1afb478026de4b7d9bea1441fa82dfbc236e836743c4d57839ec7bedea8e145e"
+    url "https://github.com/heyhuynhgiabuu/openpi/releases/download/v#{version}/OpenPi-#{version}-x64.dmg"
+  end
+
   name "OpenPi"
   desc "Desktop workbench for the Pi coding agent"
   homepage "https://github.com/heyhuynhgiabuu/openpi"
@@ -12,7 +20,6 @@ cask "openpi" do
     strategy :github_latest
   end
 
-  depends_on arch: :arm64
   depends_on macos: :big_sur
 
   app "OpenPi.app"
