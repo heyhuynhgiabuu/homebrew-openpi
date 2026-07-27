33c2ec166460a53c9d46908d5af17add89774c59
Buu <buddingnewinsights@gmail.com>
2026-07-27 14:21:37 +0700

    fix(cask): satisfy Homebrew stanza grouping

--- Casks/openpi.rb
+++ Casks/openpi.rb
@@ -3,11 +3,12 @@ cask "openpi" do
 
   on_arm do
     sha256 "a775c7b22896f0e9e10b70728a2896e0696f7919e2727f732444afb5a9ae9a8d"
+
     url "https://github.com/heyhuynhgiabuu/openpi/releases/download/v#{version}/OpenPi-#{version}-arm64.dmg"
   end
-
   on_intel do
     sha256 "1afb478026de4b7d9bea1441fa82dfbc236e836743c4d57839ec7bedea8e145e"
+
     url "https://github.com/heyhuynhgiabuu/openpi/releases/download/v#{version}/OpenPi-#{version}-x64.dmg"
   end
 
